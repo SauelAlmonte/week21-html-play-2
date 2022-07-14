5e69ac5ef9d7b6af761998ca644080eb83e85ca9
Salmonte <almontesauel@gmail.com>
2022-07-14 19:16:34 -0400

    JavaScript

---
+++ src/index.js
@@ -0,0 +1,25 @@
+document.getElementById("app").innerHTML = `
+<div class="navbar">
+        <div>
+            <p>
+                <a href="https://about.google">About</a>
+              </p>
+          
+              <p>
+                <a href="https://store.google.com">Store</a>
+              </p>
+          
+              <p>
+                <a href="https://mail.google.com">Gmail</a>
+              </p>
+          
+              <p>
+                <a href="https://www.google.com/imghp?hl=en&authuser=0&ogbl">Images</a>
+              </p>
+        </div>
+        <div>
+            <a href="#" class="base-button" id="sign-in">Sign In</a>
+            <img src="img/ampljacket.jpg" id="ampl" alt="ampl">
+        </div>
+    </div>
+    `;
